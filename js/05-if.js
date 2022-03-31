aed0f341d93a178aff84d669b7f9421804160124
Dovile-dote <80914644+Dovile-dote@users.noreply.github.com>
2022-03-31 11:08:17 +0300

    Function vidurkis

---
+++ js/05-if.js
@@ -0,0 +1,21 @@
+/*
+IF
+
+Sablonai:
+- if () {}
+- if () {} else {}
+
+Palyginimo operatoriai:
+- visi: >, <, <=, >=, ==, !=, ===, !==
+- naudotini: >, <, <=, >=, ===, !==
+- nenaudotini: ==, !=
+*/
+
+const a = 7;
+const b = 5;
+
+if (a > b) {
+    console.log('A yra daugiau uz B');
+} else {
+    console.log('A nera daugiau uz B');
+}
